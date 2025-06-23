c2f567cdbe1bb802255a76ec7dc13919d6897bfa
Nadith Dhanula <116380737+iamnadith@users.noreply.github.com>
2025-06-23 08:49:20 +0000

    @iamnadith

--- api/[path].js
+++ api/[path].js
@@ -1,21 +1,19 @@
 // api/[path].js
-
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const proxy = createProxyMiddleware({
   target: 'http://macs-mf-svr2.info:9560/Asian_Global_V3/',
   changeOrigin: true,
-  pathRewrite: (path) => {
-    // Strip /api from beginning
-    const proxiedPath = path.replace(/^\/api/, '');
-    return proxiedPath;
+  pathRewrite: {
+    '^/': '/', // keep path intact
   }
 });
 
 module.exports = (req, res) => {
-  proxy(req, res, (err) => {
+  return proxy(req, res, (err) => {
     if (err) {
-      res.status(500).end('Proxy Error: ' + err.message);
+      res.statusCode = 500;
+      res.end('Proxy error: ' + err.message);
     }
   });
 };
